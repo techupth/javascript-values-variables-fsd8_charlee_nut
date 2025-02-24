2f2236197aa5e1b72fe0fbc2f9ed875db6f30940
Nut.Thakit <natthakitzn@gmail.com>
2025-02-24 14:09:42 +0700

    feat: complete excercise

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,14 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstNamme = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const totalAmount = 15000;
+const openRestaurantCount = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalAmount = 5000;
+openRestaurantCount = 2500;
