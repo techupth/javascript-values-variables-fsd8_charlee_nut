d25ea1fab2b27edb3498d48f002d00b23dc469bc
Pongsakon <charlee2543@hotmail.com>
2025-02-24 13:42:39 +0700

    feat:add javascript

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,7 @@
 //Exercise #1: Variable Creation
 //Start coding here
-
+const userName = "Karter";
+const userLastName = "Anderson";
+const address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
 //Exercise #2: Variable Reassignment
 //Start coding here
